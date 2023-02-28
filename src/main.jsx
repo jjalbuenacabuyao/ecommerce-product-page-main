175d68a669f4e871fb7dc56ced60f8d92d621929
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-28 20:13:33 +0800

    render the header

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Header } from './containers'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>,
 )
