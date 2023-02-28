f847f077d9ec714fbeab3e98e41a666ba14d55a9
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-28 21:23:33 +0800

    markup

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Nav, Cart } from "../../components";
+import logo from "../../images/logo.svg";
+import user from "../../images/image-avatar.png"
+import styles from "./header.module.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className={styles.header}>
+      <div className={styles.toggler__logo}>
+        <button className={styles.nav__toggler}>
+          <span className="sr-only">Toggle navigation</span>
+        </button>
+        <a href="#">
+          <img src={logo} alt="Sneakers logo" />
+        </a>
+        <Nav />
+      </div>
 
-export default Header
+      <div className={styles.cart__user}>
+        <Cart />
+        <img src={user} alt="User avatar" className={styles.avatar}/>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
