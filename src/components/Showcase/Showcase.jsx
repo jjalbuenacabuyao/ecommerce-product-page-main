e608d790dad2eda0f424d4f92c891e84ae059a83
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-01 18:55:45 +0800

    created

--- src/components/Showcase/Showcase.jsx
+++ src/components/Showcase/Showcase.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./showcase.module.css";
+import product1 from "../../images/image-product-1.jpg";
+import product2 from "../../images/image-product-2.jpg";
+import product3 from "../../images/image-product-3.jpg";
+import product4 from "../../images/image-product-4.jpg";
+import next from "../../images/icon-next.svg";
+import prev from "../../images/icon-previous.svg";
 
 const Showcase = () => {
+  let [slide, setSlide] = useState(0);
+  const images = [product1, product2, product3, product4];
   return (
-    <div>Showcase</div>
-  )
-}
+    <div className={styles.showcase}>
+      {images.map((image, index) => (
+        <img
+          key={index}
+          className={styles.showcase__img}
+          src={image}
+          data-active-slide={index === slide ? true : false}
+        />
+      ))}
 
-export default Showcase
+      <button className={styles.showcase__prev} onClick={() => setSlide(slide == 0 ? 0 : --slide)}>
+        <img src={prev} alt="" />
+      </button>
+
+      <button
+        className={styles.showcase__next}
+        onClick={() => setSlide(slide == 3 ? 3 : (++slide))}
+      >
+        <img src={next} alt="" />
+      </button>
+    </div>
+  );
+};
+
+export default Showcase;
