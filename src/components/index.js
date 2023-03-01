65a28343a9d9462368aa583335f3009f5b7cbfcc
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-01 18:56:33 +0800

    export components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import Nav from "./Nav/Nav";
 import Cart from "./Cart/Cart";
+import Showcase from "./Showcase/Showcase";
+import Description from "./Description/Description"
 
 export {
   Nav,
-  Cart
+  Cart,
+  Showcase,
+  Description
 }
