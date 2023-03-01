af380daa69fa52534cc32b8b7be3005468f88aad
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-01 18:56:14 +0800

    rendered MainContent

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Header } from "./containers";
+import { Header, MainContent } from "./containers";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
+    <MainContent />
   </React.StrictMode>
 );
