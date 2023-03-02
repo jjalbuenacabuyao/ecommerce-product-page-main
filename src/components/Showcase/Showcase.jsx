5409ecb0aab252c2fe9a0f18b18e7241a1ddc136
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-02 16:17:06 +0800

    import images

--- src/components/Showcase/Showcase.jsx
+++ src/components/Showcase/Showcase.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import styles from "./showcase.module.css";
-import product1 from "../../images/image-product-1.jpg";
-import product2 from "../../images/image-product-2.jpg";
-import product3 from "../../images/image-product-3.jpg";
-import product4 from "../../images/image-product-4.jpg";
-import next from "../../images/icon-next.svg";
-import prev from "../../images/icon-previous.svg";
+import {
+  product1,
+  product2,
+  product3,
+  product4,
+  next,
+  prev,
+} from "../../images";
 
 const Showcase = () => {
   let [slide, setSlide] = useState(0);
