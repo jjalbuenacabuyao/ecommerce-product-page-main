4d5cea028934f409251efac50b30cbf18bd47e77
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-02 16:16:47 +0800

    import and export images

---
+++ src/images/index.js
@@ -0,0 +1,39 @@
+import cartIcon from "./icon-cart.svg";
+import closeIcon from "./icon-close.svg";
+import deleteIcon from "./icon-delete.svg";
+import menuIcon from "./icon-menu.svg";
+import minusIcon from "./icon-minus.svg";
+import plusIcon from "./icon-plus.svg";
+import avatar from "./image-avatar.png";
+import logo from "./logo.svg";
+import product1 from "./image-product-1.jpg";
+import product1Thumbnail from "./image-product-1-thumbnail.jpg";
+import product2 from "./image-product-2.jpg";
+import product2Thumbnail from "./image-product-2-thumbnail.jpg";
+import product3 from "./image-product-3.jpg";
+import product3Thumbnail from "./image-product-3-thumbnail.jpg";
+import product4 from "./image-product-4.jpg";
+import product4Thumbnail from "./image-product-4-thumbnail.jpg";
+import next from "./icon-next.svg";
+import prev from "./icon-previous.svg";
+
+export {
+  product1,
+  product1Thumbnail,
+  product2,
+  product2Thumbnail,
+  product3,
+  product3Thumbnail,
+  product4,
+  product4Thumbnail,
+  cartIcon,
+  closeIcon,
+  deleteIcon,
+  menuIcon,
+  minusIcon,
+  plusIcon,
+  avatar,
+  logo,
+  next,
+  prev
+}
