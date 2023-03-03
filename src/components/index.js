e091560031e81290deb66442a2a97e42c0e674bf
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-03 20:48:15 +0800

    export Order and EmptyOrder

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,15 @@
 import Nav from "./Nav/Nav";
 import Cart from "./Cart/Cart";
 import Showcase from "./Showcase/Showcase";
-import Description from "./Description/Description"
+import Description from "./Description/Description";
+import EmptyOrder from "./EmptyOrder/EmptyOrder";
+import Order from "./Order/Order"
 
 export {
   Nav,
   Cart,
   Showcase,
-  Description
+  Description,
+  EmptyOrder,
+  Order
 }
