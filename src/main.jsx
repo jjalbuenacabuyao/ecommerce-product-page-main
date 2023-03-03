6025f1e5cdd4b9b5a0c6d0d3a66c1265c121a9fc
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-03-03 20:49:18 +0800

    remove Header

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Header, MainContent } from "./containers";
+import { MainContent } from "./containers";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header />
     <MainContent />
   </React.StrictMode>
 );
